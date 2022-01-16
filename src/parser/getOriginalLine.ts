46f564278676e8a707d219f228f4614beae31985
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-01-16 19:23:35 +0100

    add getOriginalLine

---
+++ src/parser/getOriginalLine.ts
@@ -0,0 +1,12 @@
+import type { BasicSourceMapConsumer, IndexedSourceMapConsumer } from 'source-map';
+import type { SourceFile, Node } from 'typescript';
+
+export const getOriginalLine = (
+  node: Node,
+  file: SourceFile,
+  sourceMap: BasicSourceMapConsumer | IndexedSourceMapConsumer
+): number | null => {
+  const { line: sourceMapLine, character: sourceMapColumn } = file.getLineAndCharacterOfPosition(node.pos);
+  const { line: originalLine } = sourceMap.originalPositionFor({ line: sourceMapLine + 1, column: sourceMapColumn });
+  return originalLine ? originalLine + 1 : null;
+}
