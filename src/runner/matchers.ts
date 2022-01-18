31d012b7609509e37ab2223465ff6376de41799e
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-01-18 09:52:59 +0100

    matcher also return error messages

--- src/runner/matchers.ts
+++ src/runner/matchers.ts
@@ -1,19 +1,34 @@
-import isEqual  from 'lodash/isEqual';
+import isEqual from 'lodash/isEqual';
 
-type MatcherFunction = (expected: any) => boolean;
-type Matchers = Record<string, MatcherFunction>;
+type MatcherResultSuccess = { pass: true };
+type MatcherResultFail = { pass: false, messages: string[], lines: (number | null)[] };
+
+export type MatcherResultWithoutLines = MatcherResultSuccess | Omit<MatcherResultFail, 'lines'>;
+export type MatcherResult = MatcherResultSuccess | MatcherResultFail;
+
+type MatcherFunction = (expected: any) => MatcherResultWithoutLines;
+type MatcherName = 'toEqual';
+type Matchers = Record<MatcherName, MatcherFunction>;
+
+const matcherMessage = (name: MatcherName, not = false): string =>
+  `expect(received)${not ? '.not' : ''}.${name}(expected)`;
 
 export const matchers = (received: string): Matchers => ({
-  toEqual: (expected: any): boolean => {
-    // TODO: do something when returning false
-    return isEqual(received, expected);
+  toEqual: (expected: any): MatcherResultWithoutLines => {
+    const pass = isEqual(received, expected);
+    if (pass) return { pass: true };
+    return { pass: false, messages: [matcherMessage('toEqual')] };
   }
 });
 
 export const not = (matchers: Matchers): Matchers => {
-  const notMatchers: Matchers = {};
+  const notMatchers: Partial<Matchers> = {};
   for (const matcher in matchers) {
-    notMatchers[matcher] = (expected: any) => !matchers[matcher](expected);
+    notMatchers[(matcher as MatcherName)] = (expected: any) => {
+      const pass = !matchers[matcher as MatcherName](expected);
+      if (pass) return { pass: true };
+      return { pass: false, messages: [matcherMessage('toEqual', true)] };
+    };
   }
-  return notMatchers;
+  return notMatchers as Matchers;
 }
