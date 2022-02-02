385b78cfbab9710f4d213439852a201a1ba1af99
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-02-02 09:48:53 +0100

    use printer & return global success

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -3,12 +3,13 @@ import type { Line } from 'utils/Line';
 import { printFile } from 'utils/printFile';
 import { executeRuns } from './executeRuns';
 import { linesToRuns } from './linesToRun';
+import { printRunResult } from './printer';
 
-export const run = (lines: Line[], path: string) => {
+export const run = async (lines: Line[], path: string): Promise<boolean> => {
   const runs = linesToRuns(lines);
   printFile('out/runs.json', runs);
   const runResult = executeRuns(runs, path);
-  runResult.output.forEach(line => {
-    console.log(line)
-  });
+  await printRunResult(runResult);
+
+  return runResult.details.success;
 }
