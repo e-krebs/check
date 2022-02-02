40baf8e953a00d11587d459b3c54c2e494f6b6a0
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-02-02 09:47:48 +0100

    add formatErrors

--- src/runner/formatters.ts
+++ src/runner/formatters.ts
@@ -1,6 +1,10 @@
 import chalk from 'chalk';
+import { diffJson } from 'diff';
+import readline from 'node:readline';
+import { createReadStream } from 'fs';
 
 import { getFile, getFolders } from 'utils/pathHelper';
+import type { RunError } from './RunTypings';
 
 export const formatBranch = (depth: number, description: string, success?: boolean): string => {
   const output: string[] = [' '.repeat(depth * 2)];
@@ -25,3 +29,75 @@ export const formatFileResult = (path: string, success: boolean): string => {
   output.push(getFile(path));
   return output.reduce((a, b) => `${a}${b}`);
 }
+
+const formatCodeLines = async (path: string, testLine: number): Promise<string[]> => {
+  const output: string[] = [];
+  const lines: number[] = [];
+
+  if (testLine !== null) {
+    for (let current = testLine - 2; current <= testLine + 3; current++) {
+      if (current >= 0) lines.push(current);
+    }
+  }
+
+  const lineReader = readline.createInterface({ input: createReadStream(path) });
+  var lineNumber = 0;
+  for await (const line of lineReader) {
+    lineNumber++;
+    if (lines.includes(lineNumber)) {
+      const head = lineNumber === testLine
+        ? `${chalk.red('  > ')}${chalk.grey(`${lineNumber} | `)}`
+        : chalk.grey(`    ${lineNumber} | `);
+      const body = (lineNumber === testLine ? chalk.white : chalk.grey)(line);
+      output.push(`${head}${body}`);
+    }
+    if (lineNumber > Math.max(...lines)) lineReader.close();
+  };
+
+  output.push('');
+  output.push(`    ${chalk.grey('at')} ${chalk.cyan(path)}${chalk.grey(`:${testLine}`)}`);
+  output.push('');
+
+  return output;
+}
+
+const formatDiff = (expected: any, received: any): string => {
+  const diffs = diffJson(expected, received);
+
+  const output: string[] = [];
+  for (const diff of diffs) {
+    const items = diff.value.split('\n');
+    for (const item of items) {
+      if (!item) continue;
+      if (diff.added) output.push(chalk.red(`\n  + ${item}`));
+      else if (diff.removed) output.push(chalk.green(`\n  - ${item}`));
+      else output.push(chalk.grey(`\n    ${item}`));
+    }
+  }
+
+  return output.reduce((a, b) => `${a}${b}`);
+};
+
+const formatError = async (runError: RunError): Promise<string[]> => {
+  const output: string[] = [];
+  const logicalPath = runError.logicalPath.reduce((a, b) => `${a} > ${b}`);
+  output.push(chalk.redBright(`  ● ${logicalPath}`));
+  let i = 0;
+  for (const detail of runError.details) {
+    output.push('');
+    output.push(`    ${detail.message}`);
+    output.push(formatDiff(detail.expected, detail.received));
+    if (detail.line) {
+      output.push('');
+      output.push(...await formatCodeLines(runError.path, detail.line));
+    }
+    i++;
+  }
+  return output;
+}
+
+export const formatErrors = async (runErrors: RunError[]): Promise<string[]> => (
+  await Promise.all(runErrors.map(
+    async error => await formatError(error)
+  ))
+).flat();
