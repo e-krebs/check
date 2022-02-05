9ce71d1869a4936f8cc846fed2226e2315ace624
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-02-05 12:51:43 +0100

    run async directly

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,17 @@ import { getConfiguration } from 'utils/getConfiguration';
 
 import { check } from './check';
 
-
-const main = async (): Promise<void> => {
-  const { pattern } = getConfiguration();
-  new Glob(pattern, async (err, files) => {
-    if (err) {
-      console.error('Error with pattern', err);
-      process.exit(1);
-    } else {
-      const outputLevel: OutputLevel = files.length > 1 ? 'short' : 'detailed';
-      const checks = await Promise.all(files.map(
-        async file => await check(file, outputLevel)
-      ));
-      const success: boolean = checks.reduce((a, b) => a && b);
-      process.exit(success ? 0 : 1);
-    }
-  });
-}
-
-main();
+const { pattern } = getConfiguration();
+new Glob(pattern, async (err, files) => {
+  if (err) {
+    console.error('Error with pattern', err);
+    process.exit(1);
+  } else {
+    const outputLevel: OutputLevel = files.length > 1 ? 'short' : 'detailed';
+    const checks = await Promise.all(files.map(
+      async file => await check(file, outputLevel)
+    ));
+    const success: boolean = checks.reduce((a, b) => a && b);
+    process.exit(success ? 0 : 1);
+  }
+});
