c8800cfa28b127076a8bcaf5b24e231084600fe6
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-02-26 20:10:06 +0100

    make matchers generic

--- packages/check/src/runner/matchers.ts
+++ packages/check/src/runner/matchers.ts
@@ -2,32 +2,32 @@ import chalk from 'chalk';
 import isEqual from 'lodash/isEqual';
 
 type MatcherResultSuccess = { pass: true };
-export type FailDetailLight = {
+export type FailDetailLight<T> = {
   message: string;
-  expected: object;
-  received: object;
+  expected: T;
+  received: T;
 };
-export type FailDetail =  FailDetailLight & { line: number | null };
+export type FailDetail<T> = FailDetailLight<T> & { line: number | null };
 export type FailError = {
   error: unknown;
   line: number | null;
 };
-export const isDetail = (fail: FailDetailLight | FailError): fail is FailDetailLight =>
-  Boolean((fail as FailDetailLight).message);
+export const isDetail = <T>(fail: FailDetailLight<T> | FailError): fail is FailDetailLight<T> =>
+  Boolean((fail as FailDetailLight<T>).message);
 
-type MatcherResultFail<Fail extends FailDetailLight = FailDetail> = {
+type MatcherResultFail<T, Fail extends FailDetailLight<T> = FailDetail<T>> = {
   pass: false,
   details: (Fail | FailError)[]
 };
 
-export type MatcherResultWithoutLines =
+export type MatcherResultWithoutLines<T> =
   MatcherResultSuccess |
-  MatcherResultFail<FailDetailLight>;
-export type MatcherResult = MatcherResultSuccess | MatcherResultFail;
+  MatcherResultFail<T, FailDetailLight<T>>;
+export type MatcherResult<T> = MatcherResultSuccess | MatcherResultFail<T>;
 
-type MatcherFunction = (expected: object) => MatcherResultWithoutLines;
+type MatcherFunction<T> = (expected: T) => MatcherResultWithoutLines<T>;
 type MatcherName = 'toEqual';
-type Matchers = Record<MatcherName, MatcherFunction>;
+type Matchers<T> = Record<MatcherName, MatcherFunction<T>>;
 
 const matcherDescription: Record<MatcherName, string> = {
   toEqual: 'deep equality',
@@ -41,8 +41,8 @@ const matcherMessage = (name: MatcherName, not = false): string => {
   return chalk.grey(`expect(${expected})${fullName}(${received})${description}`);
 };
 
-export const matchers = (received: object): Matchers => ({
-  toEqual: (expected: object): MatcherResultWithoutLines => {
+export const matchers = <T>(received: T): Matchers<T> => ({
+  toEqual: (expected: T): MatcherResultWithoutLines<T> => {
     const pass = isEqual(received, expected);
     if (pass) return { pass: true };
     return {
@@ -56,14 +56,15 @@ export const matchers = (received: object): Matchers => ({
   }
 });
 
-export const not = (matchers: Matchers): Matchers => {
-  const notMatchers: Partial<Matchers> = {};
+export const not = <T>(matchers: Matchers<T>): Matchers<T> => {
+  const notMatchers: Partial<Matchers<T>> = {};
   for (const matcherName in matchers) {
-    notMatchers[(matcherName as MatcherName)] = (expected: object) => {
+    notMatchers[(matcherName as MatcherName)] = (expected: T) => {
       const pass = !matchers[matcherName as MatcherName](expected);
       if (pass) return { pass: true };
       return {
-        pass: false, details: [{
+        pass: false,
+        details: [{
           message: matcherMessage('toEqual', true),
           expected,
           received: expected,
@@ -71,5 +72,5 @@ export const not = (matchers: Matchers): Matchers => {
       };
     };
   }
-  return notMatchers as Matchers;
+  return notMatchers as Matchers<T>;
 };
