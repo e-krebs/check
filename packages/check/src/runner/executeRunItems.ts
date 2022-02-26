9a0c87ff0560639c8a8e2a2dfa6272bc6f921de1
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-02-26 13:36:22 +0100

    fix: wrong line being reported

--- packages/check/src/runner/executeRunItems.ts
+++ packages/check/src/runner/executeRunItems.ts
@@ -2,21 +2,15 @@ import { runInContext, type Context } from 'vm';
 
 import { getContext } from './getContext';
 import {
-  FailError, FailDetail, isDetail, MatcherResult, MatcherResultWithoutLines, FailDetailLight
+  FailError, FailDetail, isDetail, MatcherResult, MatcherResultWithoutLines
 } from './matchers';
 import { isCode, isTests, type TestBranch } from './typings';
 
-const runTests = (tests: string[], context: Context): MatcherResultWithoutLines => {
-  let pass = true;
-  const details: (FailDetailLight | FailError)[] = [];
-  for (const test of tests) {
-    const matcherResult: MatcherResult = runInContext(test, context);
-    pass = pass && matcherResult.pass;
-    if (!matcherResult.pass && matcherResult.details.length > 0) {
-      details.push(...matcherResult.details);
-    }
-  }
-  return pass ? { pass: true } : { pass: false, details };
+const runTest = (test: string, context: Context): MatcherResultWithoutLines => {
+  const matcherResult: MatcherResult = runInContext(test, context);
+  return !matcherResult.pass && matcherResult.details.length > 0
+    ? { pass: false, details: matcherResult.details }
+    : { pass: true };
 };
 
 export const executeRunItems = (
@@ -36,15 +30,17 @@ export const executeRunItems = (
       }
     }
     if (isTests(runItem)) {
-      const testResult = runTests(runItem.tests, context);
-      pass = pass && testResult.pass;
-      if (!testResult.pass && testResult.details.length > 0) {
-        for (let i = 0; i < testResult.details.length; i++) {
-          const detail = testResult.details[i];
-          if (isDetail(detail)) {
-            details.push({ ...detail, line: runItem.lines[i] });
-          } else {
-            details.push(detail);
+      for (let i = 0; i < runItem.tests.length; i++) {
+        const testResult = runTest(runItem.tests[i], context);
+        pass = pass && testResult.pass;
+        if (!testResult.pass && testResult.details.length > 0) {
+          for (let j = 0; j < testResult.details.length; j++) {
+            const detail = testResult.details[j];
+            if (isDetail(detail)) {
+              details.push({ ...detail, line: runItem.lines[i] });
+            } else {
+              details.push(detail);
+            }
           }
         }
       }
