72f8744d530bc999d420e62c54c3e38960efbb8d
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-02-26 19:53:32 +0100

    force ts-node config when running worker

--- packages/check/src/checkWorker.js
+++ packages/check/src/checkWorker.js
@@ -1,3 +1,6 @@
 const path = require('path');
-require('ts-node').register();
+require('ts-node').register({
+  transpileOnly: true,
+  'ts-node': { swc: true }
+});
 require(path.resolve(__dirname, 'checkTask.ts'));
