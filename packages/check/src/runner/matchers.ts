d6584a96ad88a89e0887d93fe2615f3ad00ea4f6
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-02-27 13:23:56 +0100

    improve matcher diff output

--- packages/check/src/runner/matchers.ts
+++ packages/check/src/runner/matchers.ts
@@ -13,10 +13,20 @@ const matcherDescription: Record<MatcherName, string> = {
   toEqual: 'deep equality',
 };
 
-const diffScore = (change: Change): number => {
-  if (change.added) return 3;
-  if (change.removed) return 2;
-  return 1;
+const sortChanges = (changes: Change[]): Change[] => {
+  const sorted: Change[] = [];
+
+  for (let i = 0; i < changes.length; i++) {
+    if (changes[i].removed && changes[i + 1].added) {
+      sorted.push(changes[i + 1]);
+      sorted.push(changes[i]);
+      i++;
+    } else {
+      sorted.push(changes[i]);
+    }
+  }
+
+  return sorted;
 };
 
 const undefinedReplacement = 'undefinedReplacementString';
@@ -28,16 +38,17 @@ const formatDiff = <T>(expected: T, received: T): string[] => {
     expected as unknown as object,
     received as unknown as object,
     { undefinedReplacement }
-  ).sort((a, b) => diffScore(b) - diffScore(a));
+  );
+  const sortedDiffs = sortChanges(diffs);
 
-  for (const diff of diffs) {
+  for (const diff of sortedDiffs) {
     const items = diff.value.split('\n');
     for (const item of items) {
       if (!item) continue;
       const itemAsString = item.replace(`"${undefinedReplacement}"`, 'undefined');
       if (diff.added) output.push(chalk.red(` + ${itemAsString} `));
       else if (diff.removed) output.push(chalk.green(` - ${itemAsString} `));
-      else output.push(chalk.grey(`    ${itemAsString} `));
+      else output.push(chalk.grey(`   ${itemAsString} `));
     }
   }
   return output;
